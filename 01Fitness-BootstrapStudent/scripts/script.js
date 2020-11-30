6a8bf23625f31549a4be0561b3629362c24c4fb1
MichaelShay1 <73658651+MichaelShay1@users.noreply.github.com>
2020-11-30 12:29:45 -0700

    Add files via upload

---
+++ 01Fitness-BootstrapStudent/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Michael Shay
+    script.js
+    11.30.2020
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
